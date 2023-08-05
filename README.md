94590fd1d857d33eea6f2a1de6f3110a8dbed533
sixmillions <liubw95@163.com>
2023-08-05 06:00:27 +0000

    feat: 静态文件 1. hono处理静态文件设置； 2. 设置 favicon.ico；

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ npm run format
 
 npm run deploy
 ```
+
+## hono
+
+### 静态文件处理
+
+> https://hono.dev/getting-started/cloudflare-workers#serve-static-files
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/cloudflare-workers'
 import s3App from './s3'
 
 const app = new Hono()
 
 app.get('/', (c) => c.text('Hello World!'))
+
+// favicon.ico
+app.get('/favicon.ico', serveStatic({ path: './favicon.ico' }))
 
 // S3 APP
 app.route('/s3', s3App)
